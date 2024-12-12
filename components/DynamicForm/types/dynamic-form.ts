26cb63750fc155295e03cdcf867c51a8179cb155
Sam Willis <info@samwilliscreative.com>
2024-12-12 21:07:07 +0000

    Update types

--- components/DynamicForm/types/dynamic-form.ts
+++ components/DynamicForm/types/dynamic-form.ts
@@ -7,8 +7,23 @@ export enum DynamicFormFieldType {
   text,
 }
 
+export interface DynamicFormValidationRule {
+  required?: boolean;
+  minChars?: number;
+}
+
+export interface DynamicFormFieldOption {
+  value: string | number;
+  label: string;
+}
+
 export type DynamicFormField = {
   name: string;
   label: string;
   type: DynamicFormFieldType;
+  options?: DynamicFormFieldOption[];
+  value: null | string | number;
+  validation?: DynamicFormValidationRule[];
+  isValid: boolean;
+  isDirty: boolean;
 };
